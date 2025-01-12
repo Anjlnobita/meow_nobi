3df10ea997bf0a8afbd9b0c531a33addd13476e7
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-12 16:58:24 +0530

    Update __init__.py

--- meow/__init__.py
+++ meow/__init__.py
@@ -1,1 +1,30 @@
- 
+ import json
+import os
+
+from meow.core.bot import nobita
+from meow.core.dir import dirr
+from meow.core.git import git
+from meow.core.userbot import userbot
+from meow.core.youtube import nobii
+from meow.misc import dbb, heroku, sudo
+
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+sudo()
+nobii()
+app = nobita()
+userbot = userbot()
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+HELPABLE = {}
