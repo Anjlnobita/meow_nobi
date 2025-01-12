ecd51eed54803e0ebe591cb075cad73d8f06a5fa
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-12 12:48:43 +0530

    Update __main__.py

--- meow/__main__.py
+++ meow/__main__.py
@@ -1,1 +1,74 @@
- 
+import asyncio
+import importlib
+from telegram.ext import Application
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+from telegram import Update
+from telegram.ext import CommandHandler, CallbackContext, MessageHandler, filters
+
+from pyrogram import idle
+
+import config
+from config import BANNED_USERS
+from VIPMUSIC import HELPABLE, LOGGER, app, userbot
+from VIPMUSIC.core.call import VIP
+from VIPMUSIC.plugins import ALL_MODULES
+from VIPMUSIC.utils.database import get_banned_users, get_gbanned
+
+application = Application.builder().token(config.BOT_TOKEN).build()
+
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER("meow").error(
+            "No Assistant Clients Vars Defined!.. Exiting Process."
+        )
+        return
+    if not config.SPOTIFY_CLIENT_ID and not config.SPOTIFY_CLIENT_SECRET:
+        LOGGER("meow").warning(
+            "No Spotify Vars defined. Your bot won't be able to play spotify queries."
+        )
+
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except Exception:
+        pass
+
+    await app.start()
+
+    for all_module in ALL_MODULES:
+        imported_module = importlib.import_module(all_module)
+
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+                HELPABLE[imported_module.__MODULE__.lower()] = imported_module
+    LOGGER("mewow.plugins").info("Successfully Imported All Modules ")
+
+    await userbot.start()
+    await nobi.start()
+    await nobi.decorators()
+    LOGGER("meow").info("meow successfully started ")
+    await idle()
+
+def main() -> None:
+    """Run bot."""
+
+
+    application.run_polling(drop_pending_updates=True)
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop_policy().get_event_loop().run_until_complete(init())
+    LOGGER("meow").info("stopping meow meow ") 
