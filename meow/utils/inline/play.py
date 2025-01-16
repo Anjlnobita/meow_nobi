a73707e40a09500ea9d88f5b43bbbbc50bd26386
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-16 15:55:39 +0530

    Update play.py

--- meow/utils/inline/play.py
+++ meow/utils/inline/play.py
@@ -1,1 +1,23 @@
- 
+import math
+
+from pyrogram.types import InlineKeyboardButton
+
+from meow.utils.formatters import time_to_seconds
+
+
+
+def stream_markup(_, chat_id):
+    buttons = [
+         [
+            InlineKeyboardButton(text="ᴘᴀᴜsᴇ", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="ʀᴇsᴜᴍᴇ", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="sᴋɪᴘ", callback_data=f"ADMIN Skip|{chat_id}")
+        ],
+        [
+            InlineKeyboardButton(text="sᴛᴏᴘ", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="ʀᴇᴘʟᴀʏ", callback_data=f"ADMIN Replay|{chat_id}"),
+        ],
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+    ]
+
+    return buttons
