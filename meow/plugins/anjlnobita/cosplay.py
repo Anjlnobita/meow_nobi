e310e40b327da156fa433488c264fed7466a502a
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-17 21:18:20 +0530

    Update cosplay.py

--- meow/plugins/anjlnobita/cosplay.py
+++ meow/plugins/anjlnobita/cosplay.py
@@ -4,17 +4,13 @@ from __main__ import telethn as meow
 
 @meow.on(events.NewMessage(pattern="^/cosplay"))
 async def waifu(event):
-  r = requests.get("https://waifu-api.vercel.app").json() #api credit- @YASH_SHARMA_1807 on telegram
-  await event.reply(file=r)
+    r = requests.get("https://waifu-api.vercel.app").json()
+    await event.reply(file=r["url"], caption="Cosplay image")
 
 @meow.on(events.NewMessage(pattern="^/lewd"))
 async def waifu(event):
-  r = requests.get("https://waifu-api.vercel.app/items/1").json()
-  await event.reply(file=r)
+    r = requests.get("https://waifu-api.vercel.app/items/1").json()
+    await event.reply(file=r["url"], caption="Lewd image")
 
 __mod_name__ = "ᴄᴏsᴘʟᴀʏ"
-__help__ = """
-Just a weeb type module to get anime cosplay and lewd pictures
-- /cosplay
-- /lewd
-"""
+__help__ = """ Just a weeb type module to get anime cosplay and lewd pictures - /cosplay - /lewd """
