547ad6d77ce95e76e563892a5f483622883b5a44
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-17 19:06:55 +0530

    Update __init__.py

--- meow/__init__.py
+++ meow/__init__.py
@@ -12,6 +12,17 @@ from .logging import LOGGER
 
 
 
+from telethon import TelegramClient
+
+
+api_id = config.API_ID
+api_hash = config.API_HASH
+bot_token = config.BOT_TOKEN
+
+
+telethn = TelegramClient("hinata", api_id, api_hash)
+
+
 
 dirr()
 git()
