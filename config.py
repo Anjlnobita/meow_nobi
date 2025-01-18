f737de43d6800797be7eee0655c81e260a1ab50a
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-18 23:51:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,135 +1,34 @@
-import re
 import os
+import re
 from os import getenv
+
 from dotenv import load_dotenv
 from pyrogram import filters
 
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
 
+API_ID = int(getenv("API_ID", ""))
+
+
+API_HASH = getenv("API_HASH")
 
 
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 
-
-MONGO_DB_URI = getenv("MONGO_DB_URI")
-
-
-
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 1000))
-
-
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
-
-
-OWNER_ID = int(getenv("OWNER_ID"))
-
-
-
-HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
-
-
-HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-
-
-UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO",
-    "https://github.com/anjlnobita/meow_nobi",
-)
-
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
-GIT_TOKEN = getenv(
-    "GIT_TOKEN", None
-)
-
-
-
-SUPPORT_CHANNEL = getenv("https://t.me/nobi_bots")
-
-SUPPORT_CHAT = getenv("https://t.me/anime_societyy")
-
-
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
-
-
-
-SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
-
-SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
-
-
-
-PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", "125"))
-
-
-
-TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
-
-
-TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
-
-
-
-STRING1 = getenv("STRING_SESSION", None)
-STRING2 = getenv("STRING_SESSION2", None)
-STRING3 = getenv("STRING_SESSION3", None)
-STRING4 = getenv("STRING_SESSION4", None)
-STRING5 = getenv("STRING_SESSION5", None)
-
-
-YTDOWNLOADER = 1
-LOG_FILE_NAME = "logs.txt"
-TEMP_DB_FOLDER = "nobita"
-BANNED_USERS = filters.user()
-adminlist = {}
-lyrical = {}
-LOG = 2
-votemode = {}
-autoclean = []
-confirmer = {}
-chatstats = {}
-userstats = {}
-clean = {}
-
-
-START_IMG_URL = getenv(
-    "START_IMG_URL", "https://envs.sh/CwU.jpg"
-)
-PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://envs.sh/CLg.jpeg"
-)
-PLAYLIST_IMG_URL = "https://envs.sh/CwU.jpg"
-STATS_IMG_URL = "https://envs.sh/CLg.jpeg"
-TELEGRAM_AUDIO_URL = "https://envs.sh/CLg.jpeg"
-TELEGRAM_VIDEO_URL = "https://envs.sh/CLg.jpeg"
-STREAM_IMG_URL = "https://envs.sh/CLg.jpeg"
-SOUNCLOUD_IMG_URL = "https://envs.sh/CLg.jpeg"
-YOUTUBE_IMG_URL = "https://envs.sh/CLg.jpeg"
-SPOTIFY_ARTIST_IMG_URL = "https://envs.sh/CLg.jpeg"
-SPOTIFY_ALBUM_IMG_URL = "https://envs.sh/CLg.jpeg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://envs.sh/CLg.jpeg"
-
-
-def time_to_seconds(time):
-    stringt = str(time)
-    return sum(int(x) * 800**i for i, x in enumerate(reversed(stringt.split(":"))))
-
-
-DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
+MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 
 CLEANMODE_DELETE_MINS = int(
-    getenv("CLEANMODE_MINS", "18000")
-)  # Remember to give value Seconds
+    getenv("CLEANMODE_MINS", "60")
+)  # Remember to give value in Seconds
 
 
 
 DURATION_LIMIT_MIN = int(
-    getenv("DURATION_LIMIT", "3000")
+    getenv("DURATION_LIMIT", "1250")
 )  # Remember to give value in Minutes
 
 
@@ -144,57 +43,239 @@ EXTRA_PLUGINS_REPO = getenv(
     "https://github.com/anjlnobita/neow",
 )
 
+
 EXTRA_PLUGINS_FOLDER = getenv("EXTRA_PLUGINS_FOLDER", "plugins")
+
+
 
 SONG_DOWNLOAD_DURATION = int(
     getenv("SONG_DOWNLOAD_DURATION_LIMIT", "1000")
-) 
+)
 
-# Token allow for access youtube songs. [ True = use token data, False = use cookies ]
+
+
+LOG_GROUP_ID = getenv("LOG_GROUP_ID", "")
+
+
+# Your User ID.
+OWNER_ID = list(
+    map(int, getenv("OWNER_ID", "").split())
+)
+
+
+
+PRIVACY_LINK = getenv(
+    "PRIVACY_LINK", ""
+)
+
+
+HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+
+
+
+UPSTREAM_REPO = getenv(
+    "UPSTREAM_REPO",
+    "https://github.com/anjlnobita/meow",
+)
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+
+
+
+GIT_TOKEN = getenv(
+    "GIT_TOKEN",
+    "",
+)
 TOKEN_ALLOW = os.getenv("TOKEN_ALLOW", "true")
 
-# Auto Gcast/Broadcast Handler, Write:- [On / Off] During Hosting.
+
 AUTO_GCAST = os.getenv("AUTO_GCAST", "on")
 
-# Auto Broadcast Message That You Want Use In Auto Broadcast In All Groups.
+
+
 AUTO_GCAST_MSG = getenv("AUTO_GCAST_MSG", "")
+
+
+
+SUPPORT_CHANNEL = getenv(
+    "SUPPORT_CHANNEL", "https://t.me/nobi_bots"
+)
+
+
+SUPPORT_GROUP = getenv(
+    "SUPPORT_GROUP", "https://t.me/anime_societyy"
+)
+
+SUPPORT_CHAT = getenv(
+    "SUPPORT_GROUP", "https://t.me/anime_societyy"
+)
+
+
+
+AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", False)
+
+
+
+AUTO_LEAVE_ASSISTANT_TIME = int(
+    getenv("ASSISTANT_LEAVE_TIME", 1800)
+) 
+
+
+
 
 PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", "False")
 
 
-# Time sleep duration For Youtube Downloader
-YOUTUBE_DOWNLOAD_EDIT_SLEEP = int(getenv("YOUTUBE_EDIT_SLEEP", "1"))
 
-# Time sleep duration For Telegram Downloader
-TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "1"))
+
+YOUTUBE_DOWNLOAD_EDIT_SLEEP = int(getenv("YOUTUBE_EDIT_SLEEP", "0.1"))
 
 
 
-# Maximum number of video calls allowed on bot. You can later set it via /set_video_limit on telegram
+TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "0.1"))
+
+
+
+GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/anjlnobita/meow")
+
+
+
+SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "19609edb1b9f4ed7be0c8c1342039362")
+
+
+SPOTIFY_CLIENT_SECRET = getenv(
+    "SPOTIFY_CLIENT_SECRET", "409e31d3ddd64af08cfcc3b0f064fcbe"
+)
+
+
+
+
 VIDEO_STREAM_LIMIT = int(getenv("VIDEO_STREAM_LIMIT", "999"))
 
 
-# Maximum Limit Allowed for users to save playlists on bot's server
-SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "500"))
 
-# MaximuM limit for fetching playlist's track from youtube, spotify, apple links.
+
+SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "10038"))
+
+
+
 PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", "500"))
 
 
-# Telegram audio  and video file size limit
-
 TG_AUDIO_FILESIZE_LIMIT = int(
     getenv("TG_AUDIO_FILESIZE_LIMIT", "1073741824")
-)  # Remember to give value in bytes
+)
+
 
 TG_VIDEO_FILESIZE_LIMIT = int(
     getenv("TG_VIDEO_FILESIZE_LIMIT", "1073741824")
-) 
+)
+
+
+
+
 
 SET_CMDS = getenv("SET_CMDS", "False")
 
 
 
+
+STRING1 = getenv("STRING_SESSION", None)
+STRING2 = getenv("STRING_SESSION2", None)
+STRING3 = getenv("STRING_SESSION3", None)
+STRING4 = getenv("STRING_SESSION4", None)
+STRING5 = getenv("STRING_SESSION5", None)
+
+
+
+
+
+### DONT TOUCH or EDIT codes after this line
+BANNED_USERS = filters.user()
+YTDOWNLOADER = 1
+LOG = 2
+LOG_FILE_NAME = "logs.txt"
+TEMP_DB_FOLDER = "tempdb"
+adminlist = {}
+lyrical = {}
+chatstats = {}
+userstats = {}
+clean = {}
+
+autoclean = []
+
+
+
+START_IMG_URL = getenv(
+    "START_IMG_URL",
+    "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg",
+)
+
+PING_IMG_URL = getenv(
+    "PING_IMG_URL",
+    "https://telegra.ph/file/91533956c91d0fd7c9f20.jpg",
+)
+
+PLAYLIST_IMG_URL = getenv(
+    "PLAYLIST_IMG_URL",
+    "https://telegra.ph/file/f4edfbd83ec3150284aae.jpg",
+)
+
+GLOBAL_IMG_URL = getenv(
+    "GLOBAL_IMG_URL",
+    "https://telegra.ph/file/de1db74efac1770b1e8e9.jpg",
+)
+
+STATS_IMG_URL = getenv(
+    "STATS_IMG_URL",
+    "https://telegra.ph/file/4dd9e2c231eaf7c290404.jpg",
+)
+
+TELEGRAM_AUDIO_URL = getenv(
+    "TELEGRAM_AUDIO_URL",
+    "https://telegra.ph/file/8234d704952738ebcda7f.jpg",
+)
+
+TELEGRAM_VIDEO_URL = getenv(
+    "TELEGRAM_VIDEO_URL",
+    "https://telegra.ph/file/8d02ff3bde400e465219a.jpg",
+)
+
+STREAM_IMG_URL = getenv(
+    "STREAM_IMG_URL",
+    "https://telegra.ph/file/e24f4a5f695ec5576a8f3.jpg",
+)
+
+SOUNCLOUD_IMG_URL = getenv(
+    "SOUNCLOUD_IMG_URL",
+    "https://telegra.ph/file/7645d1e04021323c21db9.jpg",
+)
+
+YOUTUBE_IMG_URL = getenv(
+    "YOUTUBE_IMG_URL",
+    "https://telegra.ph/file/76d29aa31c40a7f026d7e.jpg",
+)
+
+SPOTIFY_ARTIST_IMG_URL = getenv(
+    "SPOTIFY_ARTIST_IMG_URL",
+    "https://telegra.ph/file/b7758d4e1bc32aa9fb6ec.jpg",
+)
+
+SPOTIFY_ALBUM_IMG_URL = getenv(
+    "SPOTIFY_ALBUM_IMG_URL",
+    "https://telegra.ph/file/60ed85638e00df10985db.jpg",
+)
+
+SPOTIFY_PLAYLIST_IMG_URL = getenv(
+    "SPOTIFY_PLAYLIST_IMG_URL",
+    "https://telegra.ph/file/f4edfbd83ec3150284aae.jpg",
+)
+
+
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
 
 
 DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
@@ -207,8 +288,94 @@ if SUPPORT_CHANNEL:
         )
 
 
-if SUPPORT_CHAT:
+if SUPPORT_GROUP:
     if not re.match("(?:http|https)://", SUPPORT_GROUP):
         print(
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
         )
+
+
+if UPSTREAM_REPO:
+    if not re.match("(?:http|https)://", UPSTREAM_REPO):
+        print(
+            "[ERROR] - Your UPSTREAM_REPO url is wrong. Please ensure that it starts with https://"
+        )
+
+
+if GITHUB_REPO:
+    if not re.match("(?:http|https)://", GITHUB_REPO):
+        print(
+            "[ERROR] - Your GITHUB_REPO url is wrong. Please ensure that it starts with https://"
+        )
+
+
+if PING_IMG_URL:
+    if PING_IMG_URL != "https://telegra.ph/file/91533956c91d0fd7c9f20.jpg":
+        if not re.match("(?:http|https)://", PING_IMG_URL):
+            print(
+                "[ERROR] - Your PING_IMG_URL url is wrong. Please ensure that it starts with https://"
+            )
+
+
+if PLAYLIST_IMG_URL:
+    if PLAYLIST_IMG_URL != "https://telegra.ph/file/f4edfbd83ec3150284aae.jpg":
+        if not re.match("(?:http|https)://", PLAYLIST_IMG_URL):
+            print(
+                "[ERROR] - Your PLAYLIST_IMG_URL url is wrong. Please ensure that it starts with https://"
+            )
+
+
+if GLOBAL_IMG_URL:
+    if GLOBAL_IMG_URL != "https://telegra.ph/file/de1db74efac1770b1e8e9.jpg":
+        if not re.match("(?:http|https)://", GLOBAL_IMG_URL):
+            print(
+                "[ERROR] - Your GLOBAL_IMG_URL url is wrong. Please ensure that it starts with https://"
+            )
+
+
+if STATS_IMG_URL:
+    if STATS_IMG_URL != "https://telegra.ph/file/4dd9e2c231eaf7c290404.jpg":
+        if not re.match("(?:http|https)://", STATS_IMG_URL):
+            print(
+                "[ERROR] - Your STATS_IMG_URL url is wrong. Please ensure that it starts with https://"
+            )
+
+
+if TELEGRAM_AUDIO_URL:
+    if TELEGRAM_AUDIO_URL != "https://telegra.ph/file/8234d704952738ebcda7f.jpg":
+        if not re.match("(?:http|https)://", TELEGRAM_AUDIO_URL):
+            print(
+                "[ERROR] - Your TELEGRAM_AUDIO_URL url is wrong. Please ensure that it starts with https://"
+            )
+
+
+if STREAM_IMG_URL:
+    if STREAM_IMG_URL != "https://telegra.ph/file/e24f4a5f695ec5576a8f3.jpg":
+        if not re.match("(?:http|https)://", STREAM_IMG_URL):
+            print(
+                "[ERROR] - Your STREAM_IMG_URL url is wrong. Please ensure that it starts with https://"
+            )
+
+
+if SOUNCLOUD_IMG_URL:
+    if SOUNCLOUD_IMG_URL != "https://telegra.ph/file/7645d1e04021323c21db9.jpg":
+        if not re.match("(?:http|https)://", SOUNCLOUD_IMG_URL):
+            print(
+                "[ERROR] - Your SOUNCLOUD_IMG_URL url is wrong. Please ensure that it starts with https://"
+            )
+
+
+if YOUTUBE_IMG_URL:
+    if YOUTUBE_IMG_URL != "https://telegra.ph/file/76d29aa31c40a7f026d7e.jpg":
+        if not re.match("(?:http|https)://", YOUTUBE_IMG_URL):
+            print(
+                "[ERROR] - Your YOUTUBE_IMG_URL url is wrong. Please ensure that it starts with https://"
+            )
+
+
+if TELEGRAM_VIDEO_URL:
+    if TELEGRAM_VIDEO_URL != "https://telegra.ph/file/8d02ff3bde400e465219a.jpg":
+        if not re.match("(?:http|https)://", TELEGRAM_VIDEO_URL):
+            print(
+                "[ERROR] - Your TELEGRAM_VIDEO_URL url is wrong. Please ensure that it starts with https://"
+            )
