5dc1c601b0f8cac39c188dc4343370a59f511b1f
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-18 12:55:12 +0530

    Update telegraph.py

--- meow/plugins/tools/telegraph.py
+++ meow/plugins/tools/telegraph.py
@@ -1,32 +1,50 @@
 from telegraph import upload_file
 from pyrogram import filters
-from meow import app
 from pyrogram.types import InputMediaPhoto
+from meow import app
 
-
-
-@app.on_message(filters.command(["tgm" , "telegraph"]))
-def ul(_, message):
+# Command handler for the "tgm" or "telegraph" command
+@app.on_message(filters.command(["tgm", "telegraph"]))
+async def ul(_, message):
     reply = message.reply_to_message
-    if reply.media:
-        i = message.reply("𝐌𝙰𝙺𝙴 𝐀 𝐋𝙸𝙽𝙺...")
-        path = reply.download()
-        fk = upload_file(path)
-        for x in fk:
-            url = "https://telegra.ph" + x
+    if reply and reply.media:
+        try:
+            # Inform user about the process
+            i = await message.reply("𝐌𝙰𝙺𝙴 𝐀 𝐋𝙸𝙽𝙺...")
+            # Download the file (this is the media file from the reply)
+            path = await reply.download()
+            # Upload the file to Telegraph
+            fk = upload_file(path)
+            for x in fk:
+                url = f"https://telegra.ph{x}"
+                # Edit the message to show the generated link
+                await i.edit(f'Yᴏᴜʀ ʟɪɴᴋ sᴜᴄᴄᴇssғᴜʟ Gᴇɴ {url}')
+        except Exception as e:
+            # Handle errors and inform the user
+            await message.reply(f"Error occurred while processing: {str(e)}")
+    else:
+        # If the message is not a reply to a media message
+        await message.reply("Please reply to a media message to create a Telegraph link.")
 
-        i.edit(f'Yᴏᴜʀ ʟɪɴᴋ sᴜᴄᴄᴇssғᴜʟ Gᴇɴ {url}')
-
-########____________________________________________________________######
-
-@app.on_message(filters.command(["graph" , "grf"]))
-def ul(_, message):
+# Command handler for the "graph" or "grf" command
+@app.on_message(filters.command(["graph", "grf"]))
+async def ul(_, message):
     reply = message.reply_to_message
-    if reply.media:
-        i = message.reply("𝐌𝙰𝙺𝙴 𝐀 𝐋𝙸𝙽𝙺...")
-        path = reply.download()
-        fk = upload_file(path)
-        for x in fk:
-            url = "https://graph.org" + x
-
-        i.edit(f'Yᴏᴜʀ ʟɪɴᴋ sᴜᴄᴄᴇssғᴜʟ Gᴇɴ {url}')
+    if reply and reply.media:
+        try:
+            # Inform user about the process
+            i = await message.reply("𝐌𝙰𝙺𝙴 𝐀 𝐋𝙸𝙽𝙺...")
+            # Download the file (this is the media file from the reply)
+            path = await reply.download()
+            # Upload the file to Graph.org
+            fk = upload_file(path)
+            for x in fk:
+                url = f"https://graph.org{x}"
+                # Edit the message to show the generated link
+                await i.edit(f'Yᴏᴜʀ ʟɪɴᴋ sᴜᴄᴄᴇssғᴜʟ Gᴇɴ {url}')
+        except Exception as e:
+            # Handle errors and inform the user
+            await message.reply(f"Error occurred while processing: {str(e)}")
+    else:
+        # If the message is not a reply to a media message
+        await message.reply("Please reply to a media message to create a Graph link.")
