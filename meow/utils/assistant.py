f45996dfe016baa655f42e95f1bb8d207c5f5dd5
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-19 00:17:41 +0530

    Update assistant.py

--- meow/utils/assistant.py
+++ meow/utils/assistant.py
@@ -1,14 +1,6 @@
-#
-# Copyright (C) 2024 by THE-VIP-BOY-OP@Github, < https://github.com/THE-VIP-BOY-OP >.
-#
-# This file is part of < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
-#
 
-from VIPMUSIC.utils.database import get_client
+
+from meow.utils.database import get_client
 
 
 async def get_assistant_details():
