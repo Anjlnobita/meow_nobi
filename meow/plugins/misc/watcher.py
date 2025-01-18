9b1e4c8265d02c572c8f840f36f3735b218395b6
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-19 04:24:14 +0530

    Update watcher.py

--- meow/plugins/misc/watcher.py
+++ meow/plugins/misc/watcher.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from VIPMUSIC import app
-from VIPMUSIC.core.call import VIP
+from meow import app
+from meow.core.call import meow 
 
 
 @app.on_message(filters.video_chat_started, group=20)
@@ -12,7 +12,7 @@ async def force_stop_stream(_, message: Message):
     try:
         if message.left_chat_member and not message.left_chat_member is None:
             if message.left_chat_member.id == (await get_assistant(message.chat.id)).id:
-                return await VIP.force_stop_stream(message.chat.id)
-        await VIP.force_stop_stream(message.chat.id)
+                return await meow.force_stop_stream(message.chat.id)
+        await meow.force_stop_stream(message.chat.id)
     except Exception:
         pass
